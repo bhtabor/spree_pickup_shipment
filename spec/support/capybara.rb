e806fcd14ef93f55f563012424f565c3ed330183
Biruk H. Tabor <biruk.haileye@gmail.com>
2019-11-07 10:43:12 +0300

    extension setup

---
+++ spec/support/capybara.rb
@@ -0,0 +1,21 @@
+require 'capybara/rspec'
+require 'capybara-screenshot'
+require 'capybara-screenshot/rspec'
+require 'capybara/rails'
+
+RSpec.configure do
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome,
+      options: Selenium::WebDriver::Chrome::Options.new(
+        args: %w[headless disable-gpu window-size=1920,1080 --enable-features=NetworkService,NetworkServiceInProcess --disable-features=VizDisplayCompositor],
+        log_level: :error
+      )
+  end
+  Capybara.javascript_driver = :chrome
+
+  Capybara::Screenshot.register_driver(:chrome) do |driver, path|
+    driver.browser.save_screenshot(path)
+  end
+
+end
