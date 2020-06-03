5b024498a874715b2785fcebfa480446c1ab0585
Biruk H. Tabor <biruk.haileye@gmail.com>
2020-06-03 17:37:18 +0300

    add rspec shoulda matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,16 @@ require 'rspec/rails'
 require 'ffaker'
 require 'pry'
 
+require 'shoulda/matchers'
+require 'shoulda-callback-matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
--- spree_pickup_shipment.gemspec
+++ spree_pickup_shipment.gemspec
@@ -42,5 +42,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'sass-rails'
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'shoulda-matchers'
+  s.add_development_dependency 'shoulda-callback-matchers'
   s.add_development_dependency 'webdrivers'
 end
